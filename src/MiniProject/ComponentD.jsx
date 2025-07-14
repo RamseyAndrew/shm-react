be236762ddda06f226924398e8f3257855358efc
ramsey900 <ramsey.anyona@student.moringaschool.com>
2025-07-14 11:59:37 +0300

    dada

--- src/MiniProject/ComponentD.jsx
+++ src/MiniProject/ComponentD.jsx
@@ -2,7 +2,7 @@ function ComponetD() {
   return (
     <div>
       <div>
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.cAobzB0mBSlBjpBXbNRdSwHaE7%3Fpid%3DApi&f=1&ipt=7d672b2e556a29787d70cf9d6f7e8b9276106b890a62aef5e92c39d900800eac&ipo=images" />
+        <img src="https://imgd.aeplcdn.com/642x336/n/cw/ec/20865/amg-gt-exterior-right-front-three-quarter-60800.jpeg?q=80" />
       </div>
       <h5>Nice Car</h5>
       <p>This is nice</p>
